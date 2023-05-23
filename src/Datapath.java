aff406b28a95199dedba99a17f18f0a067d0822c
Zeina <zeinaayman666@gmail.com>
2023-05-23 20:33:27 +0300

    decode done

--- src/Datapath.java
+++ src/Datapath.java
@@ -11,18 +11,40 @@ public class Datapath {
         int opcode = instruction & 0b1111000000000000;
         int address1 = instruction & 0b0000111111000000;
         int address2 = instruction & 0b0000000000111111;
-        //int imm = 0;
 
-        int[] instructionArray = new int[] {opcode, 0, 0, 0};
+        int[] instructionArray = new int[] {opcode, 0, 0, 0, 0, 0};
+        //opcode, address1, value1, address2, value2, immediate
+        // 0    , 1       , 2     , 3       , 4     , 5
 
-        instructionArray[1] = Registers.getR()[address1];
 
-        if(opcode < 3 || (opcode > 4 && opcode < 8)){ //if R
-            instructionArray[2] = Registers.getR()[address2];
+        switch(opcode){
+            case 0,1,2,5,6:
+                instructionArray[2] = Registers.getR()[address1];
+                instructionArray[4] = Registers.getR()[address2];
+                break;
+
+            case 7:
+                instructionArray[1] = address1;
+                instructionArray[3] = address2;
+                break;
+
+            case 3,4,8,9:
+                instructionArray[1] = address1;
+                instructionArray[5] = address2;
+                break;
+
+            case 10:
+                instructionArray[1] = address1;
+                instructionArray[4] = DataMemory.getMemory()[address2];
+                break;
+
+            case 11:
+                instructionArray[2] = Registers.getR()[address1];
+                instructionArray[3] = address2;
+
+            default: break;
         }
-        else if(opcode > 9) { //I load and store
-            instructionArray[3] = 1;
-        }
+
         return instructionArray;
     }
 
