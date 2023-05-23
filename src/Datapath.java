dae0a1faefc10320a70d017940ae539887f618a2
Zeina <zeinaayman666@gmail.com>
2023-05-23 17:56:58 +0300

    datapath

--- src/Datapath.java
+++ src/Datapath.java
@@ -7,4 +7,13 @@ public class Datapath {
         Registers.incrementPC();
         return instruction;
     }
+
+    public void Decode(int instruction){
+        int opcode = instruction & 0b1111000000000000;
+        int R1 = instruction & 0b0000111111000000;
+        int R2 = 0;
+        int immediate = 0;
+
+
+    }
 }
