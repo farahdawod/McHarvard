dac7a6522a7de9608ba854523c27d83adadf71ca
Zeina <zeinaayman666@gmail.com>
2023-05-23 20:12:04 +0300

    decode not done

--- src/Datapath.java
+++ src/Datapath.java
@@ -1,17 +1,33 @@
 public class Datapath {
 
 
-
     public int Fetch(){
         int instruction = InstructionMemory.getMemory()[Registers.getPc()];
         Registers.incrementPC();
         return instruction;
     }
 
-    public void Decode(int instruction){
+    public int[] Decode(int instruction){
         int opcode = instruction & 0b1111000000000000;
-        int R1 = instruction & 0b0000111111000000;
-        int R2 = 0;
-        int immediate = 0;
+        int address1 = instruction & 0b0000111111000000;
+        int address2 = instruction & 0b0000000000111111;
+        //int imm = 0;
+
+        int[] instructionArray = new int[] {opcode, 0, 0, 0};
+
+        instructionArray[1] = Registers.getR()[address1];
+
+        if(opcode < 3 || (opcode > 4 && opcode < 8)){ //if R
+            instructionArray[2] = Registers.getR()[address2];
+        }
+        else if(opcode > 9) { //I load and store
+            instructionArray[3] = 1;
+        }
+        return instructionArray;
+    }
+
+    public void Execute(){
+
     }
 }
+
