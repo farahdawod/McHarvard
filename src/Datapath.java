4785547c45ec0f52f2e25deb9eb67bfea908ea60
sara_a.wasfy@hotmail.com <sara_a.wasfy@hotmail.com>
2023-05-24 19:40:29 +0300

    blablabli blablablo

--- src/Datapath.java
+++ src/Datapath.java
@@ -1,5 +1,25 @@
 public class Datapath {
-    int opcode;
+    private int opcode;
+    private static int[] instructionArray;
+
+    public int getOpcode() {
+        return opcode;
+    }
+
+    public void setOpcode(int opcode) {
+        this.opcode = opcode;
+    }
+
+    public static int[] getInstructionArray() {
+        return instructionArray;
+    }
+
+    public static void setInstructionArray(int[] instructionArray) {
+        instructionArray = instructionArray;
+    }
+
+
+
     public int Fetch(){
         int instruction = InstructionMemory.getMemory()[Registers.getPc()];
         Registers.incrementPC();
@@ -11,21 +31,17 @@ public class Datapath {
         int address1 = instruction & 0b0000111111000000;
         int address2 = instruction & 0b0000000000111111;
 
-        int[] instructionArray = new int[] {opcode, 0, 0, 0, 0, 0};
+        instructionArray = new int[] {opcode, 0, 0, 0, 0, 0};
         //opcode, address1, value1, address2, value2, immediate
         // 0    , 1       , 2     , 3       , 4     , 5
 
 
         switch(opcode){
-            case 0,1,2,5,6:
-                instructionArray[2] = Registers.getR()[address1];
-                instructionArray[4] = Registers.getR()[address2];
+            case 0,1,2,5,6,7:
+                instructionArray[2] = Registers.getR()[address1]; //v1
+                instructionArray[4] = Registers.getR()[address2]; //v2
                 break;
 
-            case 7:
-                instructionArray[1] = address1;
-                instructionArray[3] = address2;
-                break;
 
             case 3,4,8,9:
                 instructionArray[1] = address1;
@@ -50,31 +66,31 @@ public class Datapath {
     public void Execute(){
         switch (opcode){
             case 0:
-                //InstructionSetArchitecture.ADD(R1,R2);
+                InstructionSetArchitecture.ADD(instructionArray[2],instructionArray[4]);
                 break;
             case 1:
-                //InstructionSetArchitecture.SUB(R1,R2);
+                InstructionSetArchitecture.SUB(instructionArray[2],instructionArray[4]);
                 break;
             case 2:
-                //InstructionSetArchitecture.MUL(R1,R2);
+                InstructionSetArchitecture.MUL(instructionArray[2],instructionArray[4]);
                 break;
             case 3:
-                //InstructionSetArchitecture.LDI(R1,IMM);
+                InstructionSetArchitecture.LDI(instructionArray[2],instructionArray[5]);
                 break;
             case 4:
-                //InstructionSetArchitecture.BEQZ(R1,IMM);
+                InstructionSetArchitecture.BEQZ(instructionArray[2],instructionArray[5]);
                 break;
             case 5:
-                //InstructionSetArchitecture.AND(R1,R2);
+                InstructionSetArchitecture.AND(instructionArray[2],instructionArray[4]);
                 break;
             case 6:
-                //InstructionSetArchitecture.OR(R1,R2);
+                InstructionSetArchitecture.OR(instructionArray[2],instructionArray[4]);
                 break;
             case 7:
-                //InstructionSetArchitecture.JR(R1,R2);
+                InstructionSetArchitecture.JR(instructionArray[2],instructionArray[4]);
                 break;
             case 8:
-                //InstructionSetArchitecture.SLC(R1,IMM);
+                InstructionSetArchitecture.SLC(instructionArray[2],instructionArray[4]);
                 break;
             case 9:
                 //InstructionSetArchitecture.SRC(R1,IMM);
--- src/InstructionSetArchitecture.java
+++ src/InstructionSetArchitecture.java
@@ -8,11 +8,12 @@ public class InstructionSetArchitecture {
             Registers.setNegative(true);
         }
         if (temp <= 255) {
-            Registers.setR(R1, temp);
+            Registers.setR(Datapath.getInstructionArray()[1], temp);
         }
         else{
+            byte tempB=(byte) temp;
             Registers.setOverflow(true);
-
+            Registers.setR(Datapath.getInstructionArray()[1], tempB);
         }
 
 
@@ -27,11 +28,12 @@ public class InstructionSetArchitecture {
             Registers.setNegative(true);
         }
         if (temp <= 255) {
-            Registers.setR(R1, temp);
+            Registers.setR(Datapath.getInstructionArray()[1], temp);
         }
         else{
+            byte tempB=(byte) temp;
             Registers.setOverflow(true);
-
+            Registers.setR(Datapath.getInstructionArray()[1], tempB);
         }
     }
 
@@ -44,11 +46,12 @@ public class InstructionSetArchitecture {
             Registers.setNegative(true);
         }
         if (temp <= 255) {
-            Registers.setR(R1, temp);
+            Registers.setR(Datapath.getInstructionArray()[1], temp);
         }
         else{
+            byte tempB=(byte) temp;
             Registers.setOverflow(true);
-
+            Registers.setR(Datapath.getInstructionArray()[1], tempB);
         }
     }
 
@@ -61,11 +64,12 @@ public class InstructionSetArchitecture {
             Registers.setNegative(true);
         }
         if (temp <= 255) {
-            Registers.setR(R1, temp);
+            Registers.setR(Datapath.getInstructionArray()[1], temp);
         }
         else{
+            byte tempB=(byte) temp;
             Registers.setOverflow(true);
-
+            Registers.setR(Datapath.getInstructionArray()[1], tempB);
         }
     }
 
@@ -78,11 +82,12 @@ public class InstructionSetArchitecture {
             Registers.setNegative(true);
         }
         if (temp <= 255) {
-            Registers.setR(R1, temp);
+            Registers.setR(Datapath.getInstructionArray()[1], temp);
         }
         else{
+            byte tempB=(byte) temp;
             Registers.setOverflow(true);
-
+            Registers.setR(Datapath.getInstructionArray()[1], tempB);
         }
     }
 
@@ -92,46 +97,51 @@ public class InstructionSetArchitecture {
     }
 
     public static void LDI(int R1, int IMM) {
-        Registers.setR(R1, IMM);
+        Registers.setR(Datapath.getInstructionArray()[1], Datapath.getInstructionArray()[5]);
     }
 
     public static void BEQZ(int R1, int IMM) {
         if (R1 == 0) {
-            Registers.setPc(Registers.getPc() + 1 + IMM);
+            Registers.setPc(Registers.getPc() + 1 + Datapath.getInstructionArray()[5]);
         }
     }
 
     public static void SLC(int R1, int IMM) {
-        Registers.setR(R1, R1 << IMM | R1 >>> 8 - IMM);
+        Registers.setR(Datapath.getInstructionArray()[1], R1 << Datapath.getInstructionArray()[5] | R1 >>> 8 - Datapath.getInstructionArray()[5]);
     }
 
     public static void SRC(int R1, int IMM) {
-        Registers.setR(R1, R1 >>> IMM | R1 << 8 - IMM);
+        Registers.setR(Datapath.getInstructionArray()[1], R1 >>> Datapath.getInstructionArray()[5] | R1 << 8 - Datapath.getInstructionArray()[5]);
     }
 
     public static void LB(int R1, int ADDRESS) {
-        Registers.setR(R1, DataMemory.getMemory()[ADDRESS]);
+        Registers.setR(Datapath.getInstructionArray()[1], DataMemory.getMemory()[ADDRESS]);
     }
 
     public static void SB(int R1, int ADDRESS) {
-        DataMemory.setMemory(ADDRESS, R1);
+        DataMemory.setMemory(ADDRESS, Datapath.getInstructionArray()[3]);
     }
 
     public static void main(String args[]) {
-        int R1 = 32;
-        int R2 = 10;
-        int IMM = 2;
+        int R1 = 2;
+        int R2 = 233;
+        int IMM = 3;
         int ADDRESS = 22;
+        byte least= (byte)333;
+        //System.out.println(least);
         //SB(R1,ADDRESS);
         //LB(R1,ADDRESS);
-        //ADD(R1,R2);
+        //int[] a = new int[] {1, 2, 3, 4, 5, 6};
+        //Datapath.setInstructionArray(a);
+        ADD(R1,R2);
         //SUB(R1,R2);
         //MUL(R1,R2);
         //LDI(R1 , IMM);
         //BEQZ(R1,IMM);
         //JR(R1, R2);
-
-        //System.out.println(Registers.getR()[R1]);
+        //SRC(R1,IMM);
+        //SLC(R1,IMM);
+        System.out.println(Registers.getR()[R1]);
         //System.out.println(Registers.getPc());
         //System.out.println(R1);
         // System.out.println(DataMemory.getMemory()[ADDRESS]);
